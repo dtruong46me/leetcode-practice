17530bb6411f7c1193feb5b115a3ac710a856df6
Dinh Truong Phan <90291855+dtruong46me@users.noreply.github.com>
2024-05-11 00:17:16 +0700

    Create problem_0905_786.py

---
+++ prob_0905_786.py
@@ -0,0 +1,27 @@
+# 786. K-th Smallest Prime Fraction
+
+"""
+You are given a sorted integer array arr containing 1 and prime numbers, where all the integers of arr are unique. You are also given an integer k.
+
+For every i and j where 0 <= i < j < arr.length, we consider the fraction arr[i] / arr[j].
+
+Return the kth smallest fraction considered. Return your answer as an array of integers of size 2, where answer[0] == arr[i] and answer[1] == arr[j].
+
+Example 1:
+
+Input: arr = [1,2,3,5], k = 3
+Output: [2,5]
+Explanation: The fractions to be considered in sorted order are:
+1/5, 1/3, 2/5, 1/2, 3/5, and 2/3.
+The third fraction is 2/5.
+Example 2:
+
+Input: arr = [1,7], k = 1
+Output: [1,7]
+"""
+
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        new_arr = [(x/y, x, y) for x in arr for y in arr if (x!=y and x<y)]
+        new_arr = new_arr.sort(key=lambda x: x[0], x[1], x[2], desc)
+        return new_arr
